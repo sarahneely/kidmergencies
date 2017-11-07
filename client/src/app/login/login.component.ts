0e5885027b7763f6b3bea62d61edaa4568d7693e
sarahneely <sporter1990@hotmail.com>
2017-11-06 20:35:52 -0800

    starting login dropdown for the navbar. I have done the html and linked bootstrap in the css.

---
+++ client/src/app/login/login.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+import { KidmergenciesService }
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
