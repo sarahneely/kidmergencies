158feace19350b3da57cbbd9e0dd55437694d028
iam-marixo <marisolgarcia.11314@gmail.com>
2017-11-18 00:06:12 -0800

    completed the 4 emergencies boxes on the homepage

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,18 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes: Routes = [
-    {
-        path: '',
-        component: 
-    },
-    {
-        path: '',
-        component: 
-    },
-    {
-        path: '',
-        component: 
-    }
+    // {
+    //     path: '',
+    //     component:
+    // },
+    // {
+    //     path: '',
+    //     component:
+    // },
+    // {
+    //     path: '',
+    //     component:
+    // }
 ];
 
 
