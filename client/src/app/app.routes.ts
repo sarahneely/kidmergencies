dcb27c171c8734d30632e022f30c09a970dddf5f
sfitzgerald16 <misfitz616@gmail.com>
2017-11-17 20:37:59 -0800

    added registration component and reg form

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,21 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const appRoutes: Routes = [
-    {
-        path: '',
-        component: 
-    },
-    {
-        path: '',
-        component: 
-    },
-    {
-        path: '',
-        component: 
-    }
-];
+import { RegisterComponent } from './register/register.component';
+import { SplashComponent } from './splash/splash.component';
 
+const appRoutes: Routes = [
+    { path: '', component: SplashComponent },
+    { path: 'register', component: RegisterComponent },
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(appRoutes)],
