f024e12ac2172c324dff1a1b3e83687d1316e5f6
Rebecca Horwitz <rebeccahorwitz@Rebeccas-MacBook-Pro.local>
2017-11-21 11:44:57 -0800

    added semicolon in app.module.ts for one of the imports

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms'
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { RoutingModule } from './app.routes';
 
 import { AppComponent } from './app.component';
