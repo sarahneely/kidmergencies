69f953e8e99a92958917ad833228166b2c99d529
Rebecca Horwitz <rebeccahorwitz@Rebeccas-MacBook-Pro.local>
2017-11-27 20:09:08 -0800

    got rid of extra splash component, got rid of extra spaces

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,22 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { RoutingModule } from './app.routes';
-
 import { AppComponent } from './app.component';
-import { SplashComponent } from './splash/splash.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
-
 import { AddContactsComponent } from './add-contacts/add-contacts.component';
 import { ContactsAdultViewComponent } from './contacts-adult-view/contacts-adult-view.component';
-
 import { SplashPageComponent } from './splash-page/splash-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SplashPageComponent,
-    SplashComponent,
     RegisterComponent,
     LoginComponent,
     AddContactsComponent,
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,21 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
 import { RegisterComponent } from './register/register.component';
-import { SplashComponent } from './splash/splash.component';
-
+import { SplashPageComponent } from './splash-page/splash-page.component';
 import { LoginComponent } from './login/login.component';
-
 import { AddContactsComponent } from './add-contacts/add-contacts.component';
 import { ContactsAdultViewComponent } from './contacts-adult-view/contacts-adult-view.component';
 
 
 const appRoutes: Routes = [
-    { path: '', component: SplashComponent },
+    { path: '', component: SplashPageComponent },
     { path: 'register', component: RegisterComponent },
-
     { path: 'login', component: LoginComponent },
-
     { path: 'contacts-view', component: ContactsAdultViewComponent },
     { path: 'add-contacts', component: AddContactsComponent },
 
