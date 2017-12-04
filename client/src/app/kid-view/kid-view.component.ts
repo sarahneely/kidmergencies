cbbcf4b0b4c62a939f26037eae859dcc617a7cae
Jacque <ayojackay@gmail.com>
2017-12-03 18:40:36 -0800

    I added HTML, and will probably add more. Started the CSS part.

---
+++ client/src/app/kid-view/kid-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kid-view',
+  templateUrl: './kid-view.component.html',
+  styleUrls: ['./kid-view.component.css']
+})
+export class KidViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
