40af47ed960c39ca72ae2eb7007864b5dc6c5861
espinozamaria <phillyswirl002@mail.fresnostate.edu>
2017-12-09 15:53:26 -0800

    Added formcontrol for new contact info

--- client/src/app/add-contacts/add-contacts.component.ts
+++ client/src/app/add-contacts/add-contacts.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-add-contacts',
@@ -7,9 +8,48 @@ import { FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms'
   styleUrls: ['./add-contacts.component.css']
 })
 export class AddContactsComponent implements OnInit {
+  phone: string;
+  firstName: string;
+  lastName: string;
+  relationship: string;
+  image: string;
+  user: string;
+  contactForm;
+  contactsUrl: any;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) { 
+    this.createForm();
+  }
 
+  createForm() {
+    this.contactForm = this.formBuilder.group({
+      phone: '',
+      firstName: '',
+      lastName: '',
+      relationship: '',
+      image: '',
+      user: ''
+    });
+  }
+
+  onSubmit() {
+    const contact = {
+      phone: this.contactForm.get('phone').value,
+      firstName: this.contactForm.get('firstName').value,
+      lastName: this.contactForm.get('lastName').value,
+      relationship: this.contactForm.get('relationship').value,
+      image: this.contactForm.get('image').value, 
+      user: this.contactForm.get('user').value
+    };
+    
+      console.log(contact);
+      this.contactsUrl = "http://localhost:8080/api/contacts";
+      this.http.post(this.contactsUrl, contact)
+      .subscribe(data => {
+      // console.log('User logged in: ', data);
+      alert(`Contact named: ${contact.firstName} was added!`);
+    });
+  }
   ngOnInit() {
   }
 
