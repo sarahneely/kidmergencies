ba1493d7ebba96e3de5706156a6bec64059beb34
Rebecca Horwitz <rebeccahorwitz@Rebeccas-MacBook-Pro.local>
2017-12-13 12:11:56 -0800

    bit of backend I did

--- client/src/app/household-info/household-info.component.ts
+++ client/src/app/household-info/household-info.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { LoginComponent } from '../login/login.component';
+import { DialogService } from 'ng2-bootstrap-modal';
 
 @Component({
   selector: 'app-household-info',
   templateUrl: './household-info.component.html',
-  styleUrls: ['./household-info.component.css']
+  styleUrls: ['./household-info.component.css'],
 })
 export class HouseholdInfoComponent implements OnInit {
-
+  phone: string;
+  firstName: string;
+  lastName: string;
+  relationship: string;
+  user: string;
   constructor() { }
 
   ngOnInit() {
